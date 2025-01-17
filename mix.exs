692349cf17955d4c542cb285d94097584ea191d8
mruoss <695307+mruoss@users.noreply.github.com>
2025-01-17 11:58:29 +0000

    [create-pull-request] automated change

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule TemporalioCloud.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/mruoss/temporalio_cloud.ex"
-  @version "0.1.0"
+  @version "0.3.0"
 
   def project do
     [
